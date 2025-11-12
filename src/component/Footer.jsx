3c2842f525c6ff450536744836fb676cc510e1d4
thaiapp322-cpu <thaiapp322@gmail.com>
2025-11-12 13:27:20 +0700

    first commit

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -5,17 +5,14 @@ function Footer() {
     <>
       <footer className='bg-gray-600 text-white shadow-inner pt-10 py-20'>
         <div className='flex flex-col md:flex-row justify-between gap-8 mx-auto max-w-6xl'>
-            {/* 1  */}
             <div className='flex-1'>
                 <h1 className='font-bold text-xl'>Smart things</h1>
                 <p>20 Smart things with us</p>
             </div>
-            {/* 2 */}
             <div className='flex-1'>
                 <h1 className='font-bold text-xl'>About</h1>
                 <p>For people who interesting in smart things</p>
             </div>
-            {/* 3 */}
             <div className='flex-1'>
                 <h1 className='font-bold text-xl'>Contact</h1>
                 <p>Email: thaiapp322@gmail.com</p>
